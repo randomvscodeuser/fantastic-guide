0ab87deaab99c4e5029a5809ad37a6445d88c17c
randomvscodeuser <107854815+randomvscodeuser@users.noreply.github.com>
2023-05-18 19:32:21 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 video = "";
 status = "";
-
+objects = [];
 function preload(){
     
 }
@@ -16,6 +16,7 @@ function setup(){
 function start(){
     objectDetector = ml5.objectDetector('cocossd', modelLoaded);
     document.getElementById("status").innerHTML = "status: detecting objects";
+    object_name = document.getElementById("object_name").value;
 }
 
 function modelLoaded(){
@@ -25,4 +26,28 @@ function modelLoaded(){
 
 function draw(){
     image(video, 0, 0, 480, 380);
+    if(status !=""){
+        objectDetector,detect(video, gotResult);
+        for(i = 0; i < objects.length; 1++){
+            document.getElementById("status").innerHTML = "status: object detected";
+            document.getElementById("number_of_objects").innerHTML = "number of objects detected are: ";
+        fill("#FF0000");
+        percent = floor(objects[i].confidence * 100);
+        text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
+        noFill();
+        stroke("#FF0000");
+        rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        if(objects[i].label == object_name){
+            iiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii
+        }    
+    }
+    }
+}
+
+function gotResult(error, results){
+    if(error){
+        console.log(error);
+    }
+    console.log(results);
+    objects = results
 }
